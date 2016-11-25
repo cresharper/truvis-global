4eb3385048c1ff3a8eefa7d5a286e3fb9490edfc
Connor Harper <cresharper@gmail.com>
2016-11-25 00:25:01 -0500

    global commit

--- dev/js/functions.js
+++ dev/js/functions.js
@@ -1,5 +1,79 @@
 //Javascript here
 
+
+$(document).ready(function() {
+	//homepage carousel
+  $("#home-carousel").owlCarousel({
+	singleItem: true,
+    slideSpeed: 400,
+    lazyEffect: "fade",
+    loop: true,
+    autoPlay: true,
+    navigation: false,
+    dots: true
+  });
+
+   //contact form
+   $("#submit_btn").click(function(e) { 
+
+       e.preventDefault()
+
+        var proceed = true;    
+        $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function(){
+            $(this).css('border-color',''); 
+            if(!$.trim($(this).val())){ 
+                $(this).css('border-color','red');    
+                proceed = false; 
+            }
+            var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+            if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
+                $(this).css('border-color','red'); 
+                proceed = false;             
+            }   
+        });
+       
+        if(proceed) 
+        {
+            
+            post_data = {
+                'user_name'     : $('input[name=name]').val(), 
+                'user_email'    : $('input[name=email]').val(), 
+                'msg'           : $('textarea[name=message]').val()
+            };
+            
+            $.post('contact_me.php', post_data, function(response){  
+                if(response.type == 'error'){ 
+                    output = '<div class="error">'+response.text+'</div>';
+                }else{
+                    output = '<div class="success">'+response.text+'</div>';
+                    
+                    $("#contact_form  input[required=true], #contact_form textarea[required=true]").val(''); 
+                    $("#contact_form #contact_body").slideUp(); 
+                }
+                $("#contact_form #contact_results").hide().html(output).slideDown();
+            }, 'json');
+        }
+	});
+
+});
+
+//mobile menu 
+$(".mm__toggle").click(function(e) {
+
+	e.preventDefault();
+
+	if ($(".main-nav__list").hasClass("main-nav--mobile")) {
+		$(".main-nav__list").removeClass("main-nav--mobile")
+	} else {
+		$(".main-nav__list").addClass("main-nav--mobile")
+	}
+	
+});
+
+
+
+//year always stays current
 window.onload = function() {
-	console.log('Hi');
+	document.getElementById('cp-year').appendChild(document.createTextNode(new Date().getFullYear()));
 }
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,52 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 	uglify = require('gulp-uglify'),
 	sass = require('gulp-ruby-sass');
     cleanCSS = require('gulp-clean-css');
-var browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
+const child = require('child_process');
+// const gutil = require('gulp-util');
+const siteRoot = '_site';
 
+//default task
+gulp.task('default', ['scripts', 'styles', 'jekyll', 'serve']);
 
-
-gulp.task('default', ['scripts', 'styles', 'serve']);
-
-//scripts task
-//Uglifies JS
-
+//scripts task, also "Uglifies" JS
 gulp.task('scripts', function() {
 	gulp.src('dev/js/*.js')
 	.pipe(uglify())
 	.pipe(gulp.dest('build/js'))
-	.pipe(browserSync.stream());
+	// .pipe(browserSync.stream());
 });
 
+//styles build/minification
 gulp.task('styles', function() {
-	return sass('dev/scss/*.scss')
+	return sass('dev/scss/styles.scss')
 	.pipe(cleanCSS())
 	.pipe(gulp.dest('build/css'))
-	.pipe(browserSync.stream());
+	// .pipe(browserSync.stream());
 });
 
 //server + file watching 
-gulp.task('serve', ['styles'], function() {
+gulp.task('serve', function() {
 
     browserSync.init({
-        server: "./"
+        server: "_site"
     });
 
-    gulp.watch("dev/scss/*.scss", ['styles']);
-    gulp.watch('dev/js/*.js', ['scripts']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("dev/scss/*.scss", ['styles', 'jekyll']);
+    gulp.watch('dev/js/*.js', ['scripts', 'jekyll']);
+    gulp.watch('_includes/*.html', ['jekyll']).on('change', browserSync.reload);
+    gulp.watch('_site').on('change', browserSync.reload);
 
+});
+
+
+gulp.task('jekyll', function(gulpCallBack){
+    var spawn = require('child_process').spawn;
+    // After build: cleanup HTML
+    var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+
+    jekyll.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+    });
 });
